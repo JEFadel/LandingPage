5539b8e943fb6dfeb470e1d3845534ae2d24ebb3
João Elias Fadel <j.elias5@me.com>
2023-05-11 18:18:01 -0300

    classes, css e script melhorados

--- script.js
+++ script.js
@@ -81,13 +81,11 @@ logo.addEventListener('click', () => {
   location.reload();
 });
 
-
 // Funcionalidade de adicionar background no header da página
 window.addEventListener('scroll', function() {
   const header = document.querySelector('header');
   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  
-  if (scrollTop > 300) {
+  if (scrollTop > 80) {
     header.classList.add('background-header');
   } else {
     header.classList.remove('background-header');
